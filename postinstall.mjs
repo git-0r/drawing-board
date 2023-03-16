9b14beea268dc49966d9b925bf984bbae75bf2c9
git-0r <rohitdec97@gmail.com>
2023-03-16 22:36:46 +0530

    add postinstall command to file

---
+++ postinstall.mjs
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { exec } from 'child_process';
+
+exec(
+  'cd ./node_modules/fabric && node build.js modules=ALL exclude=gestures,image_filters',
+  (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error building fabricjs: ${err}`);
+      return;
+    }
+
+    console.log(`Command output: ${stdout}`);
+  }
+);
